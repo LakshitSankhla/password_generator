4aab40913c2618a7448fe8dba5831edb38731a2e
Lakshit Sankhla <51900450+LakshitSankhla@users.noreply.github.com>
2020-11-10 14:38:07 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -4,29 +4,37 @@ llist=[]
 nlist=[]
 slist=[]
 
-for i in range(3):
-    Ucase=chr(random.randint(65,90))
-    ulist.append(Ucase)
-for i in range(3):
-    Lcase=chr(random.randint(97,122))
-    llist.append(Lcase)
-for i in range(2):
-    nos=chr(random.randint(48,57))
-    nlist.append(nos)
-for i in range(2):
-    spec_list=[random.randint(32,47),random.randint(58,64),random.randint(91,95), 123,125]
-    spec_char=chr(random.choice(spec_list))
-    slist.append(spec_char)
+def pass_generator(u,l,num,s):
+    for i in range(u):
+        Ucase=chr(random.randint(65,90))
+        ulist.append(Ucase)
+    for i in range(l):
+        Lcase=chr(random.randint(97,122))
+        llist.append(Lcase)
+    for i in range(num):
+        nos=chr(random.randint(48,57))
+        nlist.append(nos)
+    for i in range(s):
+        spec_list=[random.randint(32,47),random.randint(58,64),random.randint(91,95), 123,125]
+        spec_char=chr(random.choice(spec_list))
+        slist.append(spec_char)
 
-plist=ulist+llist+nlist+slist
-random.shuffle(plist)
-password="".join(plist)
-print(password)
+    plist=ulist+llist+nlist+slist
+    random.shuffle(plist)
+    password="".join(plist)
+    print(password)
 
 
 
 
-#n=int(input("Enter Lenght of Password You Want: "))
+n=int(input("Enter Lenght of Password You Want(less than 20 characters): "))
+u,l,num,s=0,0,0,0
+while(u + l + num +s != n):
+    u=random.randint(1,5)
+    l = random.randint(1, 5)
+    num = random.randint(1, 5)
+    s = random.randint(1, 5)
 
+pass_generator(u,l,num,s)
 
 
